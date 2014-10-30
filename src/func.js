0fdc685aa2ccdccd29fc70f0d51146029d3f6035
MinHyeong Kim <romz1212@gmail.com>
2014-10-30 10:20:24 +0900

    코드리뷰 결과반영

---
+++ src/func.js
@@ -0,0 +1,40 @@
+/**
+ * @fileoverview 함수관련 메서드 모음
+ * @author FE개발팀
+ */
+
+(function(ne) {
+    'use strict';
+    /* istanbul ignore if */
+    if (!ne) {
+        ne = window.ne = {};
+    }
+
+    /**
+     * 커링 메서드
+     * @param {function()} fn
+     * @param {*} obj - this로 사용될 객체
+     * @return {function()}
+     */
+    function bind(fn, obj) {
+        var slice = Array.prototype.slice;
+
+        if (fn.bind) {
+            return fn.bind.apply(fn, slice.call(arguments, 1));
+        }
+
+        /* istanbul ignore next */
+        var args = slice.call(arguments, 2);
+
+        /* istanbul ignore next */
+        return function() {
+            /* istanbul ignore next */
+            return fn.apply(obj, args.length ? args.concat(slice.call(arguments)) : arguments);
+        };
+    }
+
+    ne.func = {
+        bind: bind
+    };
+
+})(window.ne);
--- src/object.js
+++ src/object.js
@@ -5,8 +5,57 @@
 
 (function(ne) {
     'use strict';
+    /* istanbul ignore if */
     if (!ne) {
         ne = window.ne = {};
     }
+
+    /**
+     * 데이터 객체를 확장하는 메서드 (deep copy 는 하지 않는다)
+     * @param {object} target - 확장될 객체
+     * @param {...object} objects - 프로퍼티를 복사할 객체들
+     * @return {object}
+     */
+    function extend(target, objects) {
+        var source,
+            prop,
+            hasOwnProp = Object.prototype.hasOwnProperty,
+            i, len;
+
+        for (i = 1, len = arguments.length; i < len; i++) {
+            source = arguments[i];
+            for (prop in source) {
+                if (hasOwnProp.call(source, prop)) {
+                    target[prop] = source[prop];
+                }
+            }
+        }
+        return target;
+    }
+
+    /**
+     * @type {number}
+     */
+    var lastId = 0;
+
+    /**
+     * 객체에 unique한 ID를 프로퍼티로 할당한다.
+     * @param {object} obj - ID를 할당할 객체
+     * @return {number}
+     */
+    function stamp(obj) {
+        obj.__fe_id = obj.__fe_id || ++lastId;
+        return obj.__fe_id;
+    }
+
+    function resetLastId() {
+        lastId = 0;
+    }
+
+    ne.object = {
+        extend: extend,
+        stamp: stamp,
+        resetLastId: resetLastId
+    };
 
 })(window.ne);
