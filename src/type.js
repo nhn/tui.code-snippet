ce6e5683fef9ee478468671f4a138f6a2a8b06f3
soonyoung.park <soonyoung.park@nhnent.com>
2014-11-04 11:13:20 +0900

    isFalsy 추가

--- src/type.js
+++ src/type.js
@@ -46,6 +46,12 @@
     function isBoolean(obj) {
         return Object.prototype.toString.call(obj) === '[object Boolean]';
     }
+
+    /**
+     * HTMLElement타입 검사
+     * @param {HTMLElement} html
+     * @return {Boolean} HTMLElement 인지 여부
+     */
     function isHTMLElement(html)	{
         if(typeof(HTMLElement) === 'object') {
             return(html && (html instanceof HTMLElement));
@@ -53,6 +59,14 @@
         return !!(html && html.nodeType);
     }
 
+    /**
+     *
+     * @param {*} obj 평가할 대상
+     * @returns {boolean}
+     */
+    function isFalsy(obj) {
+        return !isTruthy(obj);
+    }
     /**
      * 값이 비어있는지 확인 한다.
      * - type 이 Object 의 경우 : 값이 하나라도 있으면 false 로 간주
@@ -81,17 +95,7 @@
      * @returns {boolean}
      */
     function isNotEmpty(obj) {
-        var empty = true,
-            name;
-        if (typeof obj === 'object') {
-            for (name in obj) {
-                empty = false;
-                break;
-            }
-        } else {
-            empty = !obj;
-        }
-        return !empty;
+        return !isEmpty(obj);
     }
 
 
@@ -104,6 +108,7 @@
     ne.isNumber = isNumber;
     ne.isString = isString;
     ne.isBoolean = isBoolean;
+    ne.isHTMLElement = isHTMLElement;
     ne.isEmpty = isEmpty;
     ne.isNotEmpty = isNotEmpty;
 
