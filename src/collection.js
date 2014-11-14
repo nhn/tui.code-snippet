6defdb37eb8223572a113d777c2c76e6b3d8ac09
Sungho Kim <sungho-kim@nhnent.com>
2014-11-14 16:05:43 +0900

    forEach에서 콜백함수가 false를 리턴하면 순회종료하도록 수정, forEachArray추가

--- src/collection.js
+++ src/collection.js
@@ -11,7 +11,36 @@
     }
 
     /**
+     * 어레이나 유사어레이를 순회하며 콜백함수에 전달한다.
+     * 콜백함수가 false를 리턴하면 순회를 중료한다.
+     * @param {Array} arr
+     * @param {Function} iteratee  값이 전달될 콜백함수
+     * @param {*} [context] 콜백함수의 컨텍스트
+     * @example
+     *
+     * var sum = 0;
+     *
+     * forEach([1,2,3], function(value){
+     *     sum += value;
+     * });
+     *
+     * => sum == 6
+     */
+    var forEachArray = function(arr, iteratee, context) {
+        var index = 0,
+            len = arr.length;
+
+        for (; index < len; index++) {
+            if(iteratee.call(context || null, arr[index], index, arr) === false) {
+                break;
+            }
+        }
+    };
+
+
+    /**
      * obj에 상속된 프로퍼티를 제외한 obj의 고유의 프로퍼티만 순회하며 콜백함수에 전달한다.
+     * 콜백함수가 false를 리턴하면 순회를 중료한다.
      * @param {object} obj
      * @param {Function} iteratee  프로퍼티가 전달될 콜백함수
      * @param {*} [context] 콜백함수의 컨텍스트
@@ -29,17 +58,23 @@
 
         for(key in obj) {
             if (obj.hasOwnProperty(key)) {
-                iteratee.call(context || null, obj[key], key, obj);
+                if(iteratee.call(context || null, obj[key], key, obj) === false) {
+                    break;
+                }
             }
         }
     };
 
     /**
      * 파라메터로 전달된 객체나 어레이를 순회하며 데이터를 콜백함수에 전달한다.
+     * 유사어레이의 경우 어레이로 전환후 사용해야함.(ex2 참고)
+     * 콜백함수가 false를 리턴하면 순회를 종료한다.
      * @param {*} obj 순회할 객체
      * @param {Function} iteratee 데이터가 전달될 콜백함수
      * @param {*} [context] 콜백함수의 컨텍스트
      * @example
+     *
+     * //ex1)
      * var sum = 0;
      *
      * forEach([1,2,3], function(value){
@@ -47,22 +82,24 @@
      * });
      *
      * => sum == 6
+     *
+     * //ex2) 유사 어레이사용
+     * function sum(){
+     *     var factors = Array.prototype.slice.call(arguments); //arguments를 어레이로 변환, arguments와 같은정보를 가진 새 어레이 리턴
+     *
+     *     forEach(factors, function(value){
+     *          ......
+     *     });
+     * }
+     *
      **/
     var forEach = function(obj, iteratee, context) {
-        var key,
-            len;
-
-        if (ne.isArray(obj)) {
-            for (key = 0, len = obj.length; key < len; key++) {
-                iteratee.call(context || null, obj[key], key, obj);
-            }
-        } else {
-            ne.forEachOwnProperties(obj, iteratee, context);
-        }
+        (ne.isArray(obj) ? ne.forEachArray : ne.forEachOwnProperties)(obj, iteratee, context);
     };
 
     /**
      * 파라메터로 전달된 객체나 어레이를 순회하며 콜백을 실행한 리턴값을 배열로 만들어 리턴한다.
+     * 유사어레이의 경우 어레이로 전환후 사용해야함.(forEach example참고)
      * @param {*} obj 순회할 객체
      * @param {Function} iteratee 데이터가 전달될 콜백함수
      * @param {*} [context] 콜백함수의 컨텍스트
@@ -86,6 +123,7 @@
 
     /**
      * 파라메터로 전달된 객체나 어레이를 순회하며 콜백을 실행한 리턴값을 다음 콜백의 첫번째 인자로 넘겨준다.
+     * 유사어레이의 경우 어레이로 전환후 사용해야함.(forEach example참고)
      * @param {*} obj 순회할 객체
      * @param {Function} iteratee 데이터가 전달될 콜백함수
      * @param {*} [context] 콜백함수의 컨텍스트
@@ -120,6 +158,7 @@
     };
 
     ne.forEachOwnProperties = forEachOwnProperties;
+    ne.forEachArray = forEachArray;
     ne.forEach = forEach;
     ne.map = map;
     ne.reduce = reduce;
