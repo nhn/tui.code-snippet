54ceb552ca0cf99d738693ba4bb5c8010e936d80
kihyun.lee <kihyun.lee@nhnent.com>
2014-12-10 11:13:42 +0900

    compareJSON 추가

--- src/object.js
+++ src/object.js
@@ -74,9 +74,123 @@
         return keys;
     };
 
+
+    function compareJSON() {
+        var leftChain,
+            rightChain,
+            l = arguments.length,
+            i;
+
+        function compare2Objects (x, y) {
+            var p;
+
+            // remember that NaN === NaN returns false
+            // and isNaN(undefined) returns true
+            if (isNaN(x) && isNaN(y) && typeof x === 'number' && typeof y === 'number') {
+                return true;
+            }
+
+            // Compare primitives and functions.
+            // Check if both arguments link to the same object.
+            // Especially useful on step when comparing prototypes
+            if (x === y) {
+                return true;
+            }
+
+            // Works in case when functions are created in constructor.
+            // Comparing dates is a common scenario. Another built-ins?
+            // We can even handle functions passed across iframes
+            if ((typeof x === 'function' && typeof y === 'function') ||
+                (x instanceof Date && y instanceof Date) ||
+                (x instanceof RegExp && y instanceof RegExp) ||
+                (x instanceof String && y instanceof String) ||
+                (x instanceof Number && y instanceof Number)) {
+                return x.toString() === y.toString();
+            }
+
+            // At last checking prototypes as good a we can
+            if (!(x instanceof Object && y instanceof Object)) {
+                return false;
+            }
+
+            if (x.isPrototypeOf(y) || y.isPrototypeOf(x)) {
+                return false;
+            }
+
+            if (x.constructor !== y.constructor) {
+                return false;
+            }
+
+            if (x.prototype !== y.prototype) {
+                return false;
+            }
+
+            // check for infinitive linking loops
+            if ($.inArray(x, leftChain) > -1 || $.inArray(y, rightChain) > -1) {
+                return false;
+            }
+
+            // Quick checking of one object beeing a subset of another.
+            for (p in y) {
+                if (y.hasOwnProperty(p) !== x.hasOwnProperty(p)) {
+                    return false;
+                }
+                else if (typeof y[p] !== typeof x[p]) {
+                    return false;
+                }
+            }
+
+            for (p in x) {
+                if (y.hasOwnProperty(p) !== x.hasOwnProperty(p)) {
+                    return false;
+                }
+                else if (typeof y[p] !== typeof x[p]) {
+                    return false;
+                }
+
+                switch (typeof (x[p])) {
+                    case 'object':
+                    case 'function':
+                        leftChain.push(x);
+                        rightChain.push(y);
+
+                        if (!compare2Objects (x[p], y[p])) {
+                            return false;
+                        }
+
+                        leftChain.pop();
+                        rightChain.pop();
+                        break;
+                    default:
+                        if (x[p] !== y[p]) {
+                            return false;
+                        }
+                        break;
+                }
+            }
+
+            return true;
+        }
+
+        if (l < 1) {
+            return true;
+        }
+
+        for (i = 1; i < l; i++) {
+            leftChain = [];
+            rightChain = [];
+
+            if (!compare2Objects(arguments[0], arguments[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     ne.util.extend = extend;
     ne.util.stamp = stamp;
     ne.util._resetLastId = resetLastId;
     ne.util.keys = Object.keys || keys;
-
+    ne.util.compareJSON = compareJSON;
 })(window.ne);
