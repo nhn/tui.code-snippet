ef2defa36be1062d0e1614dd3aad19d8d3857590
minkyu-yi <minkyu.yi@nhnent.com>
2015-06-04 11:02:58 +0900

    fix code-snippet.js, code-snippet.min.js

--- code-snippet.js
+++ code-snippet.js
@@ -1,4 +1,4 @@
-/*!code-snippet v1.0.1 | NHN Entertainment*/
+/*!code-snippet v1.0.2 | NHN Entertainment*/
 /**********
  * browser.js
  **********/
@@ -1234,6 +1234,7 @@
      *          // code
      *      }
      * });
+     * @memberof ne.util
      */
     var defineNamespace = function(name, props, isOverride) {
         var namespace,
@@ -1461,6 +1462,118 @@ Enum.prototype._isEnumItem = function(key) {
 
 ne.util.Enum = Enum;
 
+})(window.ne);
+
+/**********
+ * exMap.js
+ **********/
+
+/**
+ * @fileoverview
+ * Implements the ExMap (Extended Map) object.
+ * @author NHN Ent. FE Development Team
+ * @dependency Map.js, collection.js
+ */
+
+(function(ne) {
+    'use strict';
+
+    /* istanbul ignore if */
+    if (!ne) {
+        ne = window.ne = {};
+    }
+    if (!ne.util) {
+        ne.util = window.ne.util = {};
+    }
+
+    // Caching ne.util for performance enhancing
+    var util = ne.util,
+        mapAPIsForRead = ['get', 'has', 'forEach', 'keys', 'values', 'entries'],
+        mapAPIsForDelete = ['delete', 'clear'];
+
+    /**
+     * The ExMap object is Extended Version of the ne.util.Map object.
+     * and added some useful feature to make it easy to manage the Map object.
+     * @constructor
+     * @param {Array} initData - Array of key-value pairs (2-element Arrays).
+     *      Each key-value pair will be added to the new Map
+     * @memberof ne.util
+     */
+    function ExMap(initData) {
+        this._map = new util.Map(initData);
+        this.size = this._map.size;
+    }
+
+    util.forEachArray(mapAPIsForRead, function(name) {
+        ExMap.prototype[name] = function() {
+            return this._map[name].apply(this._map, arguments);
+        };
+    });
+
+    util.forEachArray(mapAPIsForDelete, function(name) {
+        ExMap.prototype[name] = function() {
+            var result = this._map[name].apply(this._map, arguments);
+            this.size = this._map.size;
+            return result;
+        };
+    });
+
+    ExMap.prototype.set = function() {
+        this._map.set.apply(this._map, arguments);
+        this.size = this._map.size;
+        return this;
+    };
+
+    /**
+     * Sets all of the key-value pairs in the specified object to the Map object.
+     * @param  {Object} object - Plain object that has a key-value pair
+     */
+    ExMap.prototype.setObject = function(object) {
+        util.forEachOwnProperties(object, function(value, key) {
+            this.set(key, value);
+        }, this);
+    };
+
+    /**
+     * Removes the elements associated with keys in the specified array.
+     * @param  {Array} keys - Array that contains keys of the element to remove
+     */
+    ExMap.prototype.deleteByKeys = function(keys) {
+        util.forEachArray(keys, function(key) {
+            this['delete'](key);
+        }, this);
+    };
+
+    /**
+     * Sets all of the key-value pairs in the specified Map object to this Map object.
+     * @param  {Map} map - Map object to be merged into this Map object
+     */
+    ExMap.prototype.merge = function(map) {
+        map.forEach(function(value, key) {
+            this.set(key, value);
+        }, this);
+    };
+
+    /**
+     * Looks through each key-value pair in the map and returns the new ExMap object of
+     * all key-value pairs that pass a truth test implemented by the provided function.
+     * @param  {function} predicate - Function to test each key-value pair of the Map object.
+     *      Invoked with arguments (value, key). Return true to keep the element, false otherwise.
+     * @return {ExMap} A new ExMap object
+     */
+    ExMap.prototype.filter = function(predicate) {
+        var filtered = new ExMap();
+
+        this.forEach(function(value, key) {
+            if (predicate(value, key)) {
+                filtered.set(key, value);
+            }
+        });
+
+        return filtered;
+    };
+
+    util.ExMap = ExMap;
 })(window.ne);
 
 /**********
@@ -1946,6 +2059,384 @@ ne.util.Enum = Enum;
     ne.util.createObject = createObject();
     ne.util.inherit = inherit;
 
+})(window.ne);
+
+/**********
+ * map.js
+ **********/
+
+/**
+ * @fileoverview
+ * Implements the Map object.
+ * @author NHN Ent. FE Development Team
+ * @dependency type.js, collection.js
+ */
+
+(function(ne) {
+    'use strict';
+
+    /* istanbul ignore if */
+    if (!ne) {
+        ne = window.ne = {};
+    }
+    if (!ne.util) {
+        ne.util = window.ne.util = {};
+    }
+
+
+    // Caching ne.util for performance enhancing
+    var util = ne.util,
+
+    /**
+     * Using undefined for a key can be ambiguous if there's deleted item in the array,
+     * which is also undefined when accessed by index.
+     * So use this unique object as an undefined key to distinguish it from deleted keys.
+     * @private
+     * @constant
+     */
+    _KEY_FOR_UNDEFINED = {},
+
+    /**
+     * For using NaN as a key, use this unique object as a NaN key.
+     * This makes it easier and faster to compare an object with each keys in the array
+     * through no exceptional comapring for NaN.
+     */
+    _KEY_FOR_NAN = {};
+
+    /**
+     * Constructor of MapIterator
+     * Creates iterator object with new keyword.
+     * @constructor
+     * @param  {Array} keys - The array of keys in the map
+     * @param  {function} valueGetter - Function that returns certain value,
+     *      taking key and keyIndex as arguments.
+     */
+    function MapIterator(keys, valueGetter) {
+        this._keys = keys;
+        this._valueGetter = valueGetter;
+        this._length = this._keys.length;
+        this._index = -1;
+        this._done = false;
+    }
+
+    /**
+     * Implementation of Iterator protocol.
+     * @return {{done: boolean, value: *}} Object that contains done(boolean) and value.
+     */
+    MapIterator.prototype.next = function() {
+        var data = {};
+        do {
+           this._index += 1;
+       } while (util.isUndefined(this._keys[this._index]) && this._index < this._length);
+
+        if (this._index >= this._length) {
+            data.done = true;
+        } else {
+            data.done = false;
+            data.value = this._valueGetter(this._keys[this._index], this._index);
+        }
+        return data;
+    };
+
+    /**
+     * The Map object implements the ES6 Map specification as closely as possible.
+     * For using objects and primitive values as keys, this object uses array internally.
+     * So if the key is not a string, get(), set(), has(), delete() will operates in O(n),
+     * and it can cause performance issues with a large dataset.
+     *
+     * Features listed below are not supported. (can't be implented without native support)
+     * - Map object is iterable
+     * - Iterable object can be used as an argument of constructor
+     *
+     * If the browser supports full implementation of ES6 Map specification, native Map obejct
+     * will be used internally.
+     * @constructor
+     * @param  {Array} initData - Array of key-value pairs (2-element Arrays).
+     *      Each key-value pair will be added to the new Map
+     */
+    function Map(initData) {
+        this._valuesForString = {};
+        this._valuesForIndex = {};
+        this._keys = [];
+
+        if (initData) {
+            this._setInitData(initData);
+        }
+
+        this.size = 0;
+    }
+
+    /**
+     * Add all elements in the initData to the Map object.
+     * @private
+     * @param  {Array} initData - Array of key-value pairs to add to the Map object
+     */
+    Map.prototype._setInitData = function(initData) {
+        if (!util.isArray(initData)) {
+            throw new Error('Only Array is supported.');
+        }
+        util.forEachArray(initData, function(pair) {
+            this.set(pair[0], pair[1]);
+        }, this);
+    };
+
+    /**
+     * Returns true if the specified value is NaN.
+     * For unsing NaN as a key, use this method to test equality of NaN
+     * because === operator doesn't work for NaN.
+     * @private
+     * @param {*} value - Any object to be tested
+     * @return {boolean} True if value is NaN, false otherwise.
+     */
+    Map.prototype._isNaN = function(value) {
+        return typeof value === 'number' && value !== value;
+    };
+
+    /**
+     * Returns the index of the specified key.
+     * @private
+     * @param  {*} key - The key object to search for.
+     * @return {number} The index of the specified key
+     */
+    Map.prototype._getKeyIndex = function(key) {
+        var result = -1,
+            value;
+
+        if (util.isString(key)) {
+            value = this._valuesForString[key];
+            if (value) {
+                result = value.keyIndex;
+            }
+        } else {
+            result = util.inArray(key, this._keys);
+        }
+        return result;
+    };
+
+    /**
+     * Returns the original key of the specified key.
+     * @private
+     * @param  {*} key - key
+     * @return {*} Original key
+     */
+    Map.prototype._getOriginKey = function(key) {
+        var originKey = key;
+        if (key === _KEY_FOR_UNDEFINED) {
+            originKey = undefined;
+        } else if (key === _KEY_FOR_NAN) {
+            originKey = NaN;
+        }
+        return originKey;
+    };
+
+    /**
+     * Returns the unique key of the specified key.
+     * @private
+     * @param  {*} key - key
+     * @return {*} Unique key
+     */
+    Map.prototype._getUniqueKey = function(key) {
+        var uniqueKey = key;
+        if (util.isUndefined(key)) {
+            uniqueKey = _KEY_FOR_UNDEFINED;
+        } else if (this._isNaN(key)) {
+            uniqueKey = _KEY_FOR_NAN;
+        }
+        return uniqueKey;
+    };
+
+    /**
+     * Returns the value object of the specified key.
+     * @private
+     * @param  {*} key - The key of the value object to be returned
+     * @param  {number} keyIndex - The index of the key
+     * @return {{keyIndex: number, origin: *}} Value object
+     */
+    Map.prototype._getValueObject = function(key, keyIndex) {
+        if (util.isString(key)) {
+            return this._valuesForString[key];
+        } else {
+            if (util.isUndefined(keyIndex)) {
+                keyIndex = this._getKeyIndex(key);
+            }
+            if (keyIndex >= 0) {
+                return this._valuesForIndex[keyIndex];
+            }
+        }
+    };
+
+    /**
+     * Returns the original value of the specified key.
+     * @private
+     * @param  {*} key - The key of the value object to be returned
+     * @param  {number} keyIndex - The index of the key
+     * @return {*} Original value
+     */
+    Map.prototype._getOriginValue = function(key, keyIndex) {
+        return this._getValueObject(key, keyIndex).origin;
+    };
+
+    /**
+     * Returns key-value pair of the specified key.
+     * @private
+     * @param  {*} key - The key of the value object to be returned
+     * @param  {number} keyIndex - The index of the key
+     * @return {Array} Key-value Pair
+     */
+    Map.prototype._getKeyValuePair = function(key, keyIndex) {
+        return [this._getOriginKey(key), this._getOriginValue(key, keyIndex)];
+    };
+
+    /**
+     * Creates the wrapper object of original value that contains a key index
+     * and returns it.
+     * @private
+     * @param  {type} origin - Original value
+     * @param  {type} keyIndex - Index of the key
+     * @return {{keyIndex: number, origin: *}} Value object
+     */
+    Map.prototype._createValueObject = function(origin, keyIndex) {
+        return {
+            keyIndex: keyIndex,
+            origin: origin
+        };
+    };
+
+    /**
+     * Sets the value for the key in the Map object.
+     * @param  {*} key - The key of the element to add to the Map object
+     * @param  {*} value - The value of the element to add to the Map object
+     * @return {Map} The Map object
+     */
+    Map.prototype.set = function(key, value) {
+        var uniqueKey = this._getUniqueKey(key),
+            keyIndex = this._getKeyIndex(uniqueKey),
+            valueObject;
+
+        if (keyIndex < 0) {
+            keyIndex = this._keys.push(uniqueKey) - 1;
+            this.size += 1;
+        }
+        valueObject = this._createValueObject(value, keyIndex);
+
+        if (util.isString(key)) {
+            this._valuesForString[key] = valueObject;
+        } else {
+            this._valuesForIndex[keyIndex] = valueObject;
+        }
+        return this;
+    };
+
+    /**
+     * Returns the value associated to the key, or undefined if there is none.
+     * @param  {*} key - The key of the element to return
+     * @return {*} Element associated with the specified key
+     */
+    Map.prototype.get = function(key) {
+        var uniqueKey = this._getUniqueKey(key),
+            value = this._getValueObject(uniqueKey);
+
+        return value && value.origin;
+    };
+
+    /**
+     * Returns a new Iterator object that contains the keys for each element
+     * in the Map object in insertion order.
+     * @return {Iterator} A new Iterator object
+     */
+    Map.prototype.keys = function() {
+        return new MapIterator(this._keys, util.bind(this._getOriginKey, this));
+    };
+
+    /**
+     * Returns a new Iterator object that contains the values for each element
+     * in the Map object in insertion order.
+     * @return {Iterator} A new Iterator object
+     */
+    Map.prototype.values = function() {
+        return new MapIterator(this._keys, util.bind(this._getOriginValue, this));
+    };
+
+    /*
+     * Returns a new Iterator object that contains the [key, value] pairs
+     * for each element in the Map object in insertion order.
+     * @return {Iterator} A new Iterator object
+     */
+    Map.prototype.entries = function() {
+        return new MapIterator(this._keys, util.bind(this._getKeyValuePair, this));
+    };
+
+    /**
+     * Returns a boolean asserting whether a value has been associated to the key
+     * in the Map object or not.
+     * @param  {*} key - The key of the element to test for presence
+     * @return {boolean} True if an element with the specified key exists;
+     *          Otherwise false
+     */
+    Map.prototype.has = function(key) {
+        return !!this._getValueObject(key);
+    };
+
+    /**
+     * Removes the specified element from a Map object.
+     * @param {*} key - The key of the element to remove
+     */
+     // cannot use reserved keyword as a property name in IE8 and under.
+    Map.prototype['delete'] = function(key) {
+        var keyIndex;
+
+        if (util.isString(key)) {
+            if (this._valuesForString[key]) {
+                keyIndex = this._valuesForString[key].keyIndex;
+                delete this._valuesForString[key];
+            }
+        } else {
+            keyIndex = this._getKeyIndex(key);
+            if (keyIndex >= 0) {
+                delete this._valuesForIndex[keyIndex];
+            }
+        }
+
+        if (keyIndex >= 0) {
+            delete this._keys[keyIndex];
+            this.size -= 1;
+        }
+    };
+
+    /**
+     * Executes a provided function once per each key/value pair in the Map object,
+     * in insertion order.
+     * @param  {function} callback - Function to execute for each element
+     * @param  {thisArg} thisArg - Value to use as this when executing callback
+     */
+    Map.prototype.forEach = function(callback, thisArg) {
+        thisArg = thisArg || this;
+        util.forEachArray(this._keys, function(key) {
+            if (!util.isUndefined(key)) {
+                callback.call(thisArg, this._getValueObject(key).origin, key, this);
+            }
+        }, this);
+    };
+
+    /**
+     * Removes all elements from a Map object.
+     */
+    Map.prototype.clear = function() {
+        Map.call(this);
+    };
+
+    // Use native Map object if exists.
+    // But only latest versions of Chrome and Firefox support full implementation.
+    (function() {
+        var browser = util.browser;
+        if (window.Map && (
+            (browser.firefox && browser.version >= 37) ||
+            (browser.chrome && browser.version >= 42) )) {
+            Map = window.Map;
+        }
+    })();
+
+    util.Map = Map;
 })(window.ne);
 
 /**********
