3689dd44801f8cbf2890b87e8181e234c377fb2f
minkyu-yi <minkyu.yi@nhnent.com>
2015-06-17 10:34:17 +0900

    define module befroe code-review

---
+++ src/defineModule.js
@@ -0,0 +1,53 @@
+/**
+ * @fileoverview Define module
+ * @author FE Development Team
+ * @dependency defineNamespace.js, func.js, collection.js, type.js
+ */
+(function(ne) {
+
+    'use strict';
+    /* istanbul ignore if */
+    if (!ne) {
+        ne = window.ne = {};
+    }
+    /* istanbul ignore if */
+    if (!ne.util) {
+        ne.util = window.ne.util = {};
+    }
+
+    var util = ne.util,
+        INITIALIZATION_METHOD_NAME = 'initialize';
+
+    /**
+     * Define module
+     * @param {string} namespace - Namespace of module
+     * @param {Object} moduleDefinition - Object literal for module
+     * @returns {Object} Defined module
+     */
+    function defineModule(namespace, moduleDefinition) {
+        var target = util.defineNamespace(namespace),
+            base = util.extend({}, moduleDefinition);
+
+        if (util.isFunction(base[INITIALIZATION_METHOD_NAME])) {
+            base[INITIALIZATION_METHOD_NAME]();
+        }
+        base['__' + INITIALIZATION_METHOD_NAME] = base[INITIALIZATION_METHOD_NAME];
+        delete base[INITIALIZATION_METHOD_NAME];
+
+        util.forEach(base, function(item, key) {
+            if (/^[^_]\w+$/.test(key)) {
+                // public
+                if (util.isFunction(item)) {
+                    target[key] = util.bind(item, base); // method
+                } else {
+                    target[key] = item;  // property
+                }
+            }
+        });
+
+        return target
+    }
+
+    ne.util.defineModule = defineModule;
+})(window.ne);
+
---
+++ test/defineModule.test.js
@@ -0,0 +1,84 @@
+
+describe('`defineModule` function', function() {
+
+    it('should be defined', function() {
+        var defineModule = ne.util.defineModule;
+
+        expect(defineModule).toBeDefined();
+        expect(defineModule).toEqual(jasmine.any(Function));
+    });
+
+    it('should set namespace', function() {
+        var definedModule = ne.util.defineModule('foo.bar', {});
+
+        expect(window.foo).toEqual(jasmine.any(Object));
+        expect(window.foo.bar).toEqual(jasmine.any(Object));
+        expect(window.foo.bar).toBe(definedModule);
+    });
+
+    describe('return a object', function() {
+        var messagesForVerifying = {
+                privateData: "I'm private data",
+                publicData: "I'm public data",
+                privateMethod: "I'm private method",
+                publicMethod: "I'm public method",
+                initializeMethod: "initialized!!"
+            },
+            baseModule = {
+                _privateData: messagesForVerifying.privateData,
+                publicData: messagesForVerifying.publicData,
+                initializationMessage: null,
+
+                _privateMethod: function() {
+                    return messagesForVerifying.privateMethod;
+                },
+
+                initialize: function() {
+                    this.initializationMessage = messagesForVerifying.initializeMethod;
+                },
+
+                publicMethod: function() {
+                    return messagesForVerifying.publicMethod;
+                },
+
+                getPrivateData: function() {
+                    return this._privateData;
+                },
+                callPrivateMethod: function() {
+                    return this._privateMethod();
+                }
+            },
+            definedModule;
+
+        beforeEach(function() {
+            definedModule = ne.util.defineModule('foo.bar', baseModule);
+        });
+
+        it('that should not be equal to `baseModule` object', function() {
+            expect(definedModule).not.toEqual(baseModule);
+        });
+
+        it('that should have public properties', function() {
+            expect(definedModule.publicData).toBeDefined();
+            expect(definedModule.publicData).toEqual(messagesForVerifying.publicData);
+
+            expect(definedModule.publicMethod).toEqual(jasmine.any(Function));
+            expect(definedModule.publicMethod()).toEqual(messagesForVerifying.publicMethod);
+        });
+
+        it('that should be initialized', function() {
+            expect(definedModule.initializationMessage).toEqual(messagesForVerifying.initializeMethod);
+        });
+
+        it('that should not have private properties', function() {
+            expect(definedModule._privateData).not.toBeDefined();
+            expect(definedModule._privateMethod).not.toBeDefined();
+        });
+
+        it('that should have access to private properties by public properties', function() {
+            expect(definedModule.getPrivateData()).toEqual(messagesForVerifying.privateData);
+            expect(definedModule.callPrivateMethod()).toEqual(messagesForVerifying.privateMethod)
+        });
+    });
+});
+
