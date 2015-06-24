5aa6b8e703d46452c4575dd345e8ff3b6ea1c9ee
MinHyeong-Kim <minhyeong.kim@nhnent.com>
2015-06-24 18:24:11 +0900

    FIX: change some karma test configurations.

--- karma.conf.js
+++ karma.conf.js
@@ -46,7 +46,7 @@ module.exports = function(config) {
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: [
-      'dots',
+      'mocha',
       'junit',
       'coverage'
     ],
