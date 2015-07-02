7a7361b6f47c6cfd8af66cefd7ce4743c75ad021
강지웅/FE개발팀/NE <jiung.kang@nhnent.com>
2015-07-02 11:55:38 +0900

    modify example

--- src/array.js
+++ src/array.js
@@ -23,16 +23,16 @@
      * @example
      *
      *   var arr = ne.util.range(5);
-     *   alert(arr.join(',')); // 0,1,2,3,4
+     *   console.log(arr); // [0,1,2,3,4]
      *
      *   arr = ne.util.range(1, 5);
-     *   alert(arr.join(',')); // 1,2,3,4
+     *   console.log(arr); // [1,2,3,4]
      *
      *   arr = ne.util.range(2, 10, 2);
-     *   alert(arr.join(',')); 2,4,6,8
+     *   console.log(arr); // [2,4,6,8]
      *
      *   arr = ne.util.range(10, 2, -2);
-     *   alert(arr.join(',')); 10,8,6,4
+     *   console.log(arr); // [10,8,6,4]
      */
 
     function range(start, stop, step) {
