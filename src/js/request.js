af11ae88c8ebe3febef3a790c24a5467c18c5dbb
kyuwoo.choi <kyuwoo.choi@nhnent.com>
2018-04-27 16:14:56 +0900

    feat: add sendHostname

--- src/js/request.js
+++ src/js/request.js
@@ -7,6 +7,36 @@
 
 var object = require('./object');
 var collection = require('./collection');
+var type = require('./type');
+
+/**
+ * Send hostname on DOMContentLoaded.
+ * To prevent hostname set tui.usageStatistics to false.
+ * @param {string} applicationId - application id to send
+ * @ignore
+ */
+function sendHostname(applicationId) {
+    // skip only if the flag is defined and is set to false explicitly
+    if (!type.isUndefined(window.tui) && window.tui.usageStatistics === false) {
+        return;
+    }
+
+    contentLoaded(window, function() {
+        var url = 'https://www.google-analytics.com/collect';
+        var hostname = location.hostname;
+        var hitType = 'event';
+        var trackingId = 'UA-115377265-9';
+
+        imagePing(url, {
+            v: 1,
+            t: hitType,
+            tid: trackingId,
+            cid: hostname,
+            dp: hostname,
+            dh: applicationId
+        });
+    });
+}
 
 /**
  * Request image ping.
@@ -46,6 +76,48 @@ function imagePing(url, trackingInfo) {
     return trackingElement;
 }
 
+/**
+ * Cross-browser contentLoaded handler
+ * adopted from https://github.com/dperini/ContentLoaded/blob/master/src/contentloaded.js
+ * @ignore
+ */
+/* eslint-disable */
+function contentLoaded(win, fn) {
+  var done = false, top = true,
+
+      doc = win.document,
+      root = doc.documentElement,
+      modern = doc.addEventListener,
+
+      add = modern ? 'addEventListener' : 'attachEvent',
+      rem = modern ? 'removeEventListener' : 'detachEvent',
+      pre = modern ? '' : 'on',
+
+      init = function(e) {
+        if (e.type == 'readystatechange' && doc.readyState != 'complete') return;
+        (e.type == 'load' ? win : doc)[rem](pre + e.type, init, false);
+        if (!done && (done = true)) fn.call(win, e.type || e);
+      },
+
+      poll = function() {
+        try { root.doScroll('left'); } catch(e) { setTimeout(poll, 50); return; }
+        init('poll');
+      };
+
+  if (doc.readyState == 'complete') fn.call(win, 'lazy');
+  else {
+    if (!modern && root.doScroll) {
+      try { top = !win.frameElement; } catch(e) { }
+      if (top) poll();
+    }
+    doc[add](pre + 'DOMContentLoaded', init, false);
+    doc[add](pre + 'readystatechange', init, false);
+    win[add](pre + 'load', init, false);
+  }
+}
+/* eslint-enable */
+
 module.exports = {
-    imagePing: imagePing
+    imagePing: imagePing,
+    sendHostname: sendHostname
 };
