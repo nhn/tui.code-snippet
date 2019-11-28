a11b7b35191d7ab6fab65ef7dbe7021c00f5dc62
Heena Kwag <heena.kwag@nhn.com>
2019-11-28 17:44:59 +0900

    feat: add template function in domUtil

---
+++ domUtil/template.js
@@ -0,0 +1,349 @@
+/**
+ * @fileoverview Convert text by binding expressions with context.
+ * @author NHN FE Development Lab <dl_javascript@nhn.com>
+ */
+
+'use strict';
+
+var inArray = require('../array/inArray');
+var forEach = require('../collection/forEach');
+var isArray = require('../type/isArray');
+var isString = require('../type/isString');
+var extend = require('../object/extend');
+
+var EXPRESSION_REGEXP = /{{\s?(\/?[a-zA-Z0-9_.@[\] ]+)\s?}}/g;
+var BRACKET_REGEXP = /^([a-zA-Z0-9_@]+)\[([a-zA-Z0-9_@]+)\]$/;
+var NUMBER_REGEXP = /^-?\d+\.?\d*$/;
+
+var BLOCK_HELPERS = {
+  'if': handleIf,
+  'each': handleEach,
+  'with': handleWith
+};
+
+/**
+ * Find value in the context by an expression.
+ * @param {string} exp - an expression
+ * @param {object} context - context
+ * @return {*}
+ * @private
+ */
+function getValueFromContext(exp, context) {
+  var bracketExps;
+  var value = context[exp];
+
+  if (exp === 'true') {
+    value = true;
+  } else if (exp === 'false') {
+    value = false;
+  } else if (BRACKET_REGEXP.test(exp)) {
+    bracketExps = exp.split(BRACKET_REGEXP);
+    value = getValueFromContext(bracketExps[1], context)[getValueFromContext(bracketExps[2], context)];
+  } else if (NUMBER_REGEXP.test(exp)) {
+    value = parseFloat(exp);
+  }
+
+  return value;
+}
+
+/**
+ * Extract elseif and else expressions.
+ * @param {Array.<string>} ifExps - args of if expression
+ * @param {Array.<string>} sourcesInsideBlock - sources inside if block
+ * @return {object} - exps: expressions of if, elseif, and else / sourcesInsideIf: sources inside if, elseif, and else block.
+ * @private
+ */
+function extractElseif(ifExps, sourcesInsideBlock) {
+  var exps = [ifExps];
+  var sourcesInsideIf = [];
+
+  var start = 0;
+  var i, len, source;
+
+  for (i = 0, len = sourcesInsideBlock.length; i < len; i += 1) {
+    source = sourcesInsideBlock[i];
+
+    if (source.indexOf('elseif') > -1 || source === 'else') {
+      exps.push(source === 'else' ? ['true'] : source.split(' ').slice(1));
+      sourcesInsideIf.push(sourcesInsideBlock.slice(start, i));
+      start = i + 1;
+    }
+  }
+  sourcesInsideIf.push(sourcesInsideBlock.slice(start));
+
+  return {
+    exps: exps,
+    sourcesInsideIf: sourcesInsideIf
+  };
+}
+
+/**
+ * Helper function for "if". 
+ * @param {Array.<string>} exps - array of expressions split by spaces
+ * @param {Array.<string>} sourcesInsideBlock - array of sources inside the if block
+ * @param {object} context - context
+ * @return {string}
+ * @private
+ */
+function handleIf(exps, sourcesInsideBlock, context) {
+  var analyzed = extractElseif(exps, sourcesInsideBlock);
+  var result = false;
+  var compiledSource = '';
+
+  forEach(analyzed.exps, function(exp, index) {
+    result = handleExpression(exp, context);
+    if (result) {
+      compiledSource = compile(analyzed.sourcesInsideIf[index], context);
+    }
+
+    return !result;
+  });
+
+  return compiledSource;
+}
+
+/**
+ * Helper function for "each".
+ * @param {Array.<string>} exps - array of expressions split by spaces
+ * @param {Array.<string>} sourcesInsideBlock - array of sources inside the each block
+ * @param {object} context - context
+ * @return {string}
+ * @private
+ */
+function handleEach(exps, sourcesInsideBlock, context) {
+  var collection = handleExpression(exps, context);
+  var additionalKey = isArray(collection) ? '@index' : '@key';
+  var additionalContext = {};
+  var result = '';
+
+  forEach(collection, function(item, key) {
+    additionalContext[additionalKey] = key;
+    additionalContext['@this'] = item;
+    extend(additionalContext, context);
+
+    result += compile(sourcesInsideBlock.slice(), additionalContext);
+  });
+
+  return result;
+}
+
+/**
+ * Helper function for "with ... as"
+ * @param {Array.<string>} exps - array of expressions split by spaces
+ * @param {Array.<string>} sourcesInsideBlock - array of sources inside the with block
+ * @param {object} context - context
+ * @return {string}
+ * @private
+ */
+function handleWith(exps, sourcesInsideBlock, context) {
+  var asIndex = inArray('as', exps);
+  var alias = exps[asIndex + 1];
+  var result = handleExpression(exps.slice(0, asIndex), context);
+
+  var additionalContext = {};
+  additionalContext[alias] = result;
+
+  return compile(sourcesInsideBlock, extend(additionalContext, context)) || '';
+}
+
+/**
+ * Extract sources inside block in place.
+ * @param {Array.<string>} sources - array of sources
+ * @param {number} start - index of start block
+ * @param {number} end - index of end block
+ * @return {Array.<string>}
+ * @private
+ */
+function extractSourcesInsideBlock(sources, start, end) {
+  var sourcesInsideBlock = sources.splice(start + 1, end - start);
+  sourcesInsideBlock.pop();
+
+  return sourcesInsideBlock;
+}
+
+/**
+ * Concatenate the elements between previous and next of the base element in place. 
+ * @param {Array.<string>} sources - array of sources
+ * @param {number} index - index of base element
+ * @private
+ */
+function joinBetween(source, index) {
+  var start = Math.max(index - 1, 0);
+  var end = Math.min(index + 1, source.length - 1);
+  var deleteCount = end - start + 1;
+  var result = source.splice(start, deleteCount).join('');
+
+  if (deleteCount < 3) {
+    source.splice(start, 0, '', result);
+  } else {
+    source.splice(start, 0, result);
+  }
+}
+
+/**
+ * Handle block helper function
+ * @param {string} helperKeyword - helper keyword (ex. if, each, with)
+ * @param {Array.<string>} sourcesToEnd - array of sources after the starting block
+ * @param {object} context - context
+ * @return {Array.<string>}
+ * @private
+ */
+function handleBlockHelper(helperKeyword, sourcesToEnd, context) {
+  var executeBlockHelper = BLOCK_HELPERS[helperKeyword];
+  var startBlockIndices = [];
+  var helperCount = 0;
+  var index = 0;
+  var expression = sourcesToEnd[index];
+  var startBlockIndex;
+
+  do {
+    if (expression.indexOf(helperKeyword) === 0) {
+      helperCount += 1;
+      startBlockIndices.push(index);
+    } else if (expression.indexOf('/' + helperKeyword) === 0) {
+      helperCount -= 1;
+      startBlockIndex = startBlockIndices.pop();
+
+      sourcesToEnd[startBlockIndex] = executeBlockHelper(
+        sourcesToEnd[startBlockIndex].split(' ').slice(1),
+        extractSourcesInsideBlock(sourcesToEnd, startBlockIndex, index),
+        context
+      );
+      joinBetween(sourcesToEnd, startBlockIndex);
+      index = startBlockIndex - 2;
+    }
+
+    index += 2;
+    expression = sourcesToEnd[index];
+  } while (helperCount !== 0 && isString(expression));
+
+  if (helperCount !== 0) {
+    throw Error(helperKeyword + ' needs {{/' + helperKeyword + '}} expression.');
+  }
+
+  return sourcesToEnd;
+}
+
+/**
+ * Helper function for "custom helper".
+ * If helper is not a function, return helper itself.
+ * @param {Array.<string>} exps - array of expressions split by spaces (first element: helper)
+ * @param {object} context - context
+ * @return {string}
+ * @private
+ */
+function handleExpression(exps, context) {
+  var result = getValueFromContext(exps[0], context);
+
+  if (result instanceof Function) {
+    result = executeFunction(result, exps.slice(1), context);
+  }
+
+  return result;
+}
+
+/**
+ * Execute a helper function.
+ * @param {Function} helper - helper function
+ * @param {Array.<string>} argExps - expressions of arguments
+ * @param {object} context - context
+ * @return {string} - result of executing the function with arguments
+ * @private
+ */
+function executeFunction(helper, argExps, context) {
+  var args = [];
+  forEach(argExps, function(exp) {
+    args.push(getValueFromContext(exp, context));
+  });
+
+  return helper.apply(null, args);
+}
+
+/**
+ * Get a result of compiling an expression with the context.
+ * @param {Array.<string>} sources - array of sources split by regexp of expression.
+ * @param {object} context - context
+ * @return {Array.<string>} - array of sources that bind with its context
+ * @private
+ */
+function compile(sources, context) {
+  var index = 1;
+  var expression = sources[index];
+  var exps, firstExp, result;
+
+  while (isString(expression)) {
+    exps = expression.split(' ');
+    firstExp = exps[0];
+
+    if (BLOCK_HELPERS[firstExp]) {
+      result = handleBlockHelper(firstExp, sources.splice(index, sources.length - index), context);
+      sources = sources.concat(result);
+    } else {
+      sources[index] = handleExpression(exps, context);
+    }
+
+    index += 2;
+    expression = sources[index];
+  }
+
+  return sources.join('');
+}
+
+/**
+ * Convert text by binding expressions with context.
+ * <br>
+ * If expression exists in the context, it will be replaced.
+ * ex) '{{title}}' with context {title: 'Hello!'} is converted to 'Hello!'.
+ * <br>
+ * If replaced expression is a function, next expressions will be arguments of the function.
+ * ex) '{{add 1 2}}' with context {add: function(a, b) {return a + b;}} is converted to '3'.
+ * <br>
+ * It has 3 predefined block helpers '{{helper ...}} ... {{/helper}}': 'if', 'each', 'with ... as ...'.
+ * 1) 'if' evaluates conditional statements. It can use with 'elseif' and 'else'.
+ * 2) 'each' iterates an array or object. It provides '@index'(array), '@key'(object), and '@this'(current element).
+ * 3) 'with ... as ...' provides an alias.
+ * @param {string} text - text with expressions
+ * @param {object} context - context
+ * @return {string} - text that bind with its context
+ * @memberof module:domUtil
+ * @example
+ * var template = require('tui-code-snippet/domUtil/template');
+ * 
+ * var source = 
+ *     '<h1>'
+ *   +   '{{if isValidNumber title}}'
+ *   +     '{{title}}th'
+ *   +   '{{elseif isValidDate title}}'
+ *   +     'Date: {{title}}'
+ *   +   '{{/if}}'
+ *   + '</h1>'
+ *   + '{{each list}}'
+ *   +   '{{with addOne @index as idx}}'
+ *   +     '<p>{{idx}}: {{@this}}</p>'
+ *   +   '{{/with}}'
+ *   + '{{/each}}';
+ * 
+ * var context = {
+ *   isValidDate: function(text) {
+ *     return /^\d{4}-(0|1)\d-(0|1|2|3)\d$/.test(text);
+ *   },
+ *   isValidNumber: function(text) {
+ *     return /^\d+$/.test(text);
+ *   }
+ *   title: '2019-11-25',
+ *   list: ['Clean the room', 'Wash the dishes'],
+ *   addOne: function(num) {
+ *     return num + 1;
+ *   }
+ * };
+ * 
+ * var result = template(source, context);
+ * console.log(result); // <h1>Date: 2019-11-25</h1><p>1: Clean the room</p><p>2: Wash the dishes</p>
+ */
+function template(text, context) {
+  text = text.replace(/\n\s*/g, '');
+
+  return compile(text.split(EXPRESSION_REGEXP), context);
+}
+
+module.exports = template;
---
+++ test/template.spec.js
@@ -0,0 +1,245 @@
+'use strict';
+
+var template = require('../domUtil/template');
+
+describe('{{expression}}', function() {
+  it('should bind expressions with the context.', function() {
+    var source = '<div class="{{className}}"><p>{{content}}</p></div>';
+    var context = {
+      className: 'container',
+      content: 'Hello, world!'
+    };
+    expect(template(source, context)).toBe('<div class="container"><p>Hello, world!</p></div>');
+
+    source = '<span>{{ content }}</span>';
+    expect(template(source, context)).toBe('<span>Hello, world!</span>');
+
+    source = '<h3>{{title}}</h3>';
+    expect(template(source, context)).toBe('<h3></h3>');
+  });
+
+  it('should bind with number if value is a number.', function() {
+    expect(template('<p>{{ 3 }}</p>', {})).toBe('<p>3</p>');
+    expect(template('<p>{{123.4567}}</p>', {})).toBe('<p>123.4567</p>');
+  });
+
+  it('should access the value with brackets if value is an object or array.', function() {
+    expect(template('<p>{{ arr[2] }}</p>', {arr: [0, 1, 2]})).toBe('<p>2</p>');
+    expect(template('<p>{{obj[key]}}</p>', {
+      obj: {key: 'value'},
+      key: 'key'
+    })).toBe('<p>value</p>');
+    expect(template('{{each nums}}{{nums[@index]}}{{/each}}', {nums: [1, 2, 3]})).toBe('123');
+  });
+
+  it('should bind with boolean if value is "true" or "false".', function() {
+    expect(template('<p>{{ false }}</p>', {})).toBe('<p>false</p>');
+    expect(template('<p>{{true}}</p>', {})).toBe('<p>true</p>');
+  });
+});
+
+describe('{{helper arg1 arg2}}', function() {
+  it('should execute a custom helper function with arguments.', function() {
+    var source = '<div class="{{getClassNamesByStatus disabled prefix}}"></div><div class="{{getClassNamesByStatus enabled}}"></div>';
+    var context = {
+      disabled: true,
+      enabled: false,
+      prefix: 'item-',
+      getClassNamesByStatus: function(disabled, prefix) {
+        return disabled ? prefix + 'disabled' : '';
+      }
+    };
+    expect(template(source, context)).toBe('<div class="item-disabled"></div><div class=""></div>');
+
+    source = '<p>{{getZero}}</p>';
+    context = {
+      getZero: function() {
+        return '0';
+      }
+    };
+    expect(template(source, context)).toBe('<p>0</p>');
+  });
+
+  it('should bind with the first expression if passed helper is not a function.', function() {
+    var source = '<h1>{{notFunction notArg1 notArg2}}</h1>';
+    var context = {
+      notFunction: 'it is not a function',
+      notArg1: 'it is not an argument1'
+    };
+    expect(template(source, context)).toBe('<h1>it is not a function</h1>');
+
+    source = '<h2>{{notFunction notArg1 notArg2}}</h2>';
+    context = {};
+    expect(template(source, context)).toBe('<h2></h2>');
+  });
+});
+
+describe('block helper', function() {
+  it('should throw an error when endblock is missing.', function() {
+    expect(function() {
+      return template('{{if 1}}', {});
+    }).toThrowError('if needs {{/if}} expression.');
+
+    expect(function() {
+      return template('{{each arr}}', {});
+    }).toThrowError('each needs {{/each}} expression.');
+
+    expect(function() {
+      return template('{{with 1 as one}}', {});
+    }).toThrowError('with needs {{/with}} expression.');
+  });
+});
+
+describe('{{if ...}} {{elseif ...}} {{else}} ... {{/if}}', function() {
+  it('should use if expression as a helper function.', function() {
+    var source = '<div>{{if content}}<p>{{content}}</p>{{/if}}</div>';
+    expect(template(source, {content: 'Hello, world!'})).toBe('<div><p>Hello, world!</p></div>');
+    expect(template(source, {content: ''})).toBe('<div></div>');
+    expect(template(source, {})).toBe('<div></div>');
+
+    source = '{{if content}}<p>Hello, world!</p>{{/if}}';
+    expect(template(source, {content: true})).toBe('<p>Hello, world!</p>');
+    expect(template(source, {})).toBe('');
+
+    source = '{{if equals two 2}}<p>Hello, world!</p>{{/if}}';
+    expect(template(source, {
+      two: 2,
+      equals: function(a, b) {
+        return a === b;
+      }
+    })).toBe('<p>Hello, world!</p>');
+  });
+
+  it('should use elseif, else expression.', function() {
+    var source = '<p>{{if equals n 1}}one{{else}}other{{/if}}</p>';
+    var context = {
+      equals: function(a, b) {
+        return a === b;
+      }
+    };
+    context.n = 0;
+    expect(template(source, context)).toBe('<p>other</p>');
+    context.n = 1;
+    expect(template(source, context)).toBe('<p>one</p>');
+
+    source = '<p>{{if equals n 1}}{{n}} is one{{ elseif equals n 2 }}{{n}} is two{{else}}{{n}} is other{{/if}}</p>';
+    context.n = 1;
+    expect(template(source, context)).toBe('<p>1 is one</p>');
+    context.n = 4;
+    expect(template(source, context)).toBe('<p>4 is other</p>');
+    context.n = 2;
+    expect(template(source, context)).toBe('<p>2 is two</p>');
+  });
+
+  it('should use each expression in the if expression.', function() {
+    var source = '<p>{{if isNumber arr[0]}}Number: {{each arr}}{{@this}}{{/each}}{{elseif isString arr[0]}}String: {{each arr}}{{@this}}{{/each}}{{else}}Nothing{{/if}}</p>';
+    var context = {
+      isNumber: function(n) {
+        return typeof n === 'number';
+      },
+      isString: function(c) {
+        return typeof c === 'string';
+      }
+    };
+    context.arr = [1, 2, 3];
+    expect(template(source, context)).toBe('<p>Number: 123</p>');
+    context.arr = ['a', 'b', 'c'];
+    expect(template(source, context)).toBe('<p>String: abc</p>');
+    context.arr = [];
+    expect(template(source, context)).toBe('<p>Nothing</p>');
+  });
+
+  it('should use if expressions in the if expression.', function() {
+    var source = '{{if true}}It is {{if false}}False{{else}}True{{/if}}{{/if}}';
+    var context = {};
+    expect(template(source, context)).toBe('It is True');
+
+    source = 'It is {{if smaller n 5}}smaller than 5 and {{if equals n 2 }}same as 2 {{else}}not same as 2 and {{if equals n 3}}same as 3 {{/if}}{{/if}}{{else}}bigger than 5 and {{if equals n 5}}same as 5 {{else}}not same as 5 {{/if}}{{/if}}';
+    context = {
+      smaller: function(a, b) {
+        return a < b;
+      },
+      equals: function(a, b) {
+        return a === b;
+      },
+      n: 3
+    };
+    expect(template(source, context)).toBe('It is smaller than 5 and not same as 2 and same as 3 ');
+  });
+});
+
+describe('{{each ...}} @this @index @key {{/each}}', function() {
+  it('should use each expression as a helper function.', function() {
+    var source = '{{each alphabets}}<p>{{content}}</p>{{/each}}';
+    expect(template(source, {
+      alphabets: ['A', 'B', 'C'],
+      content: 'Paragraph'
+    })).toBe('<p>Paragraph</p><p>Paragraph</p><p>Paragraph</p>');
+
+    source = '{{each alphabets}}<p>{{@index}}</p>{{/each}}';
+    expect(template(source, {
+      alphabets: ['A', 'B', 'C']
+    })).toBe('<p>0</p><p>1</p><p>2</p>');
+
+    source = '{{each alphabets}}<p>{{@this}}</p>{{/each}}';
+    expect(template(source, {
+      alphabets: ['A', 'B', 'C']
+    })).toBe('<p>A</p><p>B</p><p>C</p>');
+
+    source = '{{each alphabets}}<p>{{@key}}: {{@this}}</p>{{/each}}';
+    expect(template(source, {
+      alphabets: {
+        'A': '1st',
+        'B': '2nd',
+        'C': '3rd'
+      }
+    })).toBe('<p>A: 1st</p><p>B: 2nd</p><p>C: 3rd</p>');
+
+    source = '{{each getPositiveNumbersSmallerThanFive 3}}<p>{{@this}}</p>{{/each}}';
+    expect(template(source, {
+      getPositiveNumbersSmallerThanFive: function(n) {
+        return [1, 2, 3, 4, 5].slice(0, n);
+      }
+    })).toBe('<p>1</p><p>2</p><p>3</p>');
+  });
+
+  it('should use if expression in the each expression.', function() {
+    var source = '{{each numbers}}<p>{{@this}}{{if equals @this 2}} is even{{elseif equals @this 0}} is zero{{else}} is odd{{/if}}</p>{{/each}}';
+    expect(template(source, {
+      numbers: [1, 2, 0],
+      equals: function(a, b) {
+        return parseInt(a, 10) === parseInt(b, 10);
+      }
+    })).toBe('<p>1 is odd</p><p>2 is even</p><p>0 is zero</p>');
+  });
+
+  it('should use each expressions in the each expression.', function() {
+    var source = '{{each first}}{{each second}}{{@this}}{{/each}}{{/each}}';
+    var context = {
+      first: [1, 2, 3],
+      second: [4, 5, 6]
+    };
+    expect(template(source, context)).toBe('456456456');
+  });
+});
+
+describe('{{with ... as ...}} ... {{/with}}', function() {
+  it('should be make an alias.', function() {
+    var source = '{{with content as c}}<p>{{c}}</p>{{/with}}';
+    expect(template(source, {
+      content: 'Hello, world!'
+    })).toBe('<p>Hello, world!</p>');
+
+    source = '{{with getNumberInEnglish 1 as n}}<p>{{n}}</p>{{/with}}';
+    expect(template(source, {
+      getNumberInEnglish: function(num) {
+        switch (num) {
+          case 1:
+            return 'one';
+          default:
+            return 'bigger than one';
+        }
+      }
+    })).toBe('<p>one</p>');
+  });
+});
